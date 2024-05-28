ba1d55ca969a88436388e92add9e9e564bb727fd
Jordan <jordan.lewis0676@outlook.com>
2024-05-28 07:58:30 -0500

    first commit

---
+++ DatingApp/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] 
+    public class UsersController: ControllerBase
+    {
+        private DataContext _dataContext;
+        public UsersController(DataContext context) {
+            this._dataContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> getUsers()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+            return users;
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _dataContext.Users.FindAsync(id);
+        }
+    }
+}

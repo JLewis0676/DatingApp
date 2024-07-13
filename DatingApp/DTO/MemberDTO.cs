2fe4f08cc423358adb5a3022224dd5034d90b0f6
JLewis0676 <jordan.lewis0676@outlook.com>
2024-07-13 07:26:43 -0500

    created DTOs for returning user and photo information

---
+++ DatingApp/DTO/MemberDTO.cs
@@ -0,0 +1,31 @@
+﻿using DatingApp.Entities;
+using DatingApp.Extensions;
+
+namespace DatingApp.DTO
+{
+    public class MemberDTO
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PhotoUrl { get; set; }
+
+        public int age { get; set; }
+        public string KnowAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.UtcNow;
+
+        public DateTime LastActive { get; set; } = DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introduction { get; set; }
+
+        public string LookingFor { get; set; }
+
+        public string Interests { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public List<PhotoDTO> Photos { get; set; }
+
+    }
+}

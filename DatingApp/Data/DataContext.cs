096823a79bf63ae013ca508c8c7ac6fa716baf8a
JLewis0676 <jordan.lewis0676@outlook.com>
2024-07-13 07:25:32 -0500

    introducting EF relationships

--- DatingApp/Data/DataContext.cs
+++ DatingApp/Data/DataContext.cs
@@ -1,5 +1,7 @@
 ﻿using DatingApp.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using System.Reflection.Metadata;
 
 namespace DatingApp.Data
 {
@@ -8,6 +10,19 @@ namespace DatingApp.Data
         public DataContext(DbContextOptions options): base(options) { }
 
         public DbSet<AppUser> Users { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppUser>()
+                .HasMany(e => e.Photos)
+                .WithOne(e => e.AppUser)
+                .HasForeignKey(e => e.AppUserId)
+                .IsRequired();
+            modelBuilder.Entity<Photo>()
+                .HasOne(e => e.AppUser)
+                .WithMany(e => e.Photos)
+                .HasForeignKey(e => e.AppUserId)
+                .IsRequired();
+        }
     }
 }
 

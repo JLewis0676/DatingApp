efa1f20038aa363e8a961399b610206c24ca2078
JLewis0676 <jordan.lewis0676@outlook.com>
2024-07-13 07:27:01 -0500

    attemp to use auto-mapper

---
+++ DatingApp/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,14 @@
+﻿//using DatingApp.DTO;
+using DatingApp.Entities;
+
+namespace DatingApp.Helpers
+{
+    public class AutoMapperProfiles //: Profile
+    {
+        //public AutoMapperProfiles()
+        //{
+        //    CreateMap<AppUser, MemberDTO>();
+        //    CreateMap<Photo, PhotoDTO>();
+        //}
+    }
+}

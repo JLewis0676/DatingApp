154fd26de9435bee046270f36edbd9881725722c
JLewis0676 <jordan.lewis0676@outlook.com>
2024-07-13 07:27:37 -0500

    introduced repository pattern

---
+++ DatingApp/Data/UserRepository.cs
@@ -0,0 +1,114 @@
+﻿using DatingApp.DTO;
+using DatingApp.Entities;
+using DatingApp.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+
+namespace DatingApp.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        public DataContext _context { get; }
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<AppUser> GetUserByIdAsync(int id)
+        {
+             return await _context.Users.FindAsync(id) ;
+        }
+
+        public async Task<MemberDTO> GetUserByUsernameAsync(string username) { 
+            //return await _context.Users
+            //    .Include(p=>p.Photos)
+            //    .SingleOrDefaultAsync(x=> x.UserName == username);
+
+            //    return new { Results = await _context.Users
+            //            .Select(x => new MemberDTO
+            //            {
+            //                UserName = x.UserName,
+            //                Id = x.Id,
+            //                age = x.GetAge(),
+            //                KnowAs = x.KnowAs,
+            //                Photos = x.Photos.Select(c => new PhotoDTO { Id = c.Id, Url = c.Url, IsMain = c.isMain }).ToList()
+            //            }) ;
+            //}
+
+          return await (from x in _context.Users
+                   where x.UserName == username
+                   select new MemberDTO
+                   {
+                       UserName = x.UserName,
+                       Id = x.Id,
+                       age = x.GetAge(),
+                       KnowAs = x.KnowAs,
+                       Created = x.Created,
+                       Introduction = x.Introduction,
+                       LookingFor =  x.LookingFor,
+                       Interests = x.Interests,
+                       City = x.City,
+                       Country = x.Country,
+                       Gender = x.Gender,
+                       Photos = (from c in x.Photos
+                                            select new PhotoDTO
+                                            {
+                                                Id = c.Id,
+                                                Url = c.Url,
+                                                IsMain = c.isMain
+                                            }
+                                 ).ToList(),
+                    PhotoUrl =  x.Photos.FirstOrDefault().Url
+
+                       //x.Photos.Select(c => new PhotoDTO { Id = c.Id, Url = c.Url, IsMain = c.isMain })
+                   }).SingleOrDefaultAsync();
+            
+
+        }
+
+        public async Task<IEnumerable<MemberDTO>> GetUsersAsync()
+        {
+            //return await _context.Users
+            //    .Include(p=>p.Photos)
+
+            //    .ToListAsync();
+            return await (from x in _context.Users
+                                select new MemberDTO
+                                {
+                                    UserName = x.UserName,
+                                    Id = x.Id,
+                                    age = x.GetAge(),
+                                    KnowAs = x.KnowAs,
+                                    Created = x.Created,
+                                    Introduction = x.Introduction,
+                                    LookingFor = x.LookingFor,
+                                    Interests = x.Interests,
+                                    City = x.City,
+                                    Country = x.Country,
+                                    Photos = (from c in x.Photos
+                                              select new PhotoDTO
+                                              {
+                                                  Id = c.Id,
+                                                  Url = c.Url,
+                                                  IsMain = c.isMain
+                                              }
+                                              ).ToList(),
+
+                                PhotoUrl = x.Photos.FirstOrDefault().Url
+                                    //x.Photos.Select(c => new PhotoDTO { Id = c.Id, Url = c.Url, IsMain = c.isMain })
+                                }).ToListAsync();
+        }
+
+        public async Task<bool> SaveAllAsync()
+        {
+            return await _context.SaveChangesAsync() > 0;
+        }
+
+        public void Update(AppUser user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+    }
+}
---
+++ DatingApp/Interfaces/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using DatingApp.DTO;
+using DatingApp.Entities;
+
+namespace DatingApp.Interfaces
+{
+    public interface IUserRepository
+    {
+        void Update(AppUser user);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<MemberDTO>> GetUsersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<MemberDTO> GetUserByUsernameAsync(string username);
+
+    }
+}
